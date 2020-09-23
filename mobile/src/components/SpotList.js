e8a70eaa2ef99e56b02a49f5d12589d716d178ab
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-22 22:54:29 -0300

    Mobile - Booking

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState} from 'react';
+import { withNavigation } from 'react-navigation';
 import { View, StyleSheet, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
-export default function SpotList({ tech }) {
+function SpotList({ tech, navigation }) {
     const [spots, setSpots] = useState([]);
 
     useEffect(() => {
@@ -13,11 +14,14 @@ export default function SpotList({ tech }) {
             });
 
             setSpots(response.data);
-
         }
 
         loadSpots();
     },[]);
+
+    function handleNavigate(id) {
+        navigation.navigate('Book', { id });
+    }
 
     return (
         <View style={styles.container}>
@@ -33,7 +37,7 @@ export default function SpotList({ tech }) {
                         <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
                         <Text style={styles.company}>{item.company}</Text>
                         <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity style={styles.button} onPress={() => {}}>
+                        <TouchableOpacity style={styles.button} onPress={() => handleNavigate(item._id)}>
                             <Text style={styles.buttonText}>Solicitar reserva</Text>
                         </TouchableOpacity>
                     </View>
@@ -78,4 +82,36 @@ const styles = StyleSheet.create({
 
     },
 
+    company: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+        marginTop: 10,
+
+    },
+
+    price: {
+        fontSize: 15,
+        color: '#999',
+        marginTop: 5
+    },
+
+    button: {
+        height: 32,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        marginTop: 15,
+
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+
 });
+
+export default withNavigation(SpotList);
--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,6 +1,102 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, View, StyleSheet, TextInput, Text, TouchableOpacity, AsyncStorage, Alert } from 'react-native';
 
-export default function Book() {
-    return <View />
+import api from '../services/api';
+
+export default function Book({ navigation }) {
+    const [date, setDate] = useState('');
+    const id = navigation.getParam('id');
+
+    async function handleSubmit() {
+        const user_id = await AsyncStorage.getItem('user');
+
+        await api.post(`/spots/${id}/bookings`, {
+            date
+        }, {
+            headers: { user_id }
+        })
+
+        Alert.alert('Solicitação de reserva enviada.');
+
+        navigation.navigate('List');
+
+    }
+
+    function handleCancel() {
+        navigation.navigate('List');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>DATA DE INTERESSE *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Qual data você quer reservar?"
+                placeholderTextColor="#999"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+
+            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                <Text style={styles.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: StatusBar.currentHeight,
+        margin: 30,
+
+    },
+
+    label: {
+        marginTop: 30,
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+
+    },
+
+    cancelButton: {
+        marginTop: 10,
+        backgroundColor: '#ccc',
+        
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+
+    },
+
+})
